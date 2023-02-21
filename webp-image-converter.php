5790ccb411dc7e93352cdde6964c811979f50014
Chigozie Orunta <chigozieorunta@yahoo.com>
2023-02-21 14:44:33 +0100

    Plugin comment block description

--- webp-image-converter.php
+++ webp-image-converter.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ * Plugin Name: WebP Image Converter
+ * Plugin URI:  https://github.com/chigozieorunta/webp-image-converter
+ * Description: A simple WP plugin to help convert JPG/PNG images during page runtime to WebP formats.
+ * Version:     1.0.0
+ * Author:      Chigozie Orunta
+ * Author URI:  https://chigozieorunta.com
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: webpimageconverter
+ * Domain Path: /languages
+ *
+ * @package WebPImageConverter
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Require autoload.
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
 
 // Run plugin.
-( \WebPImageConverter::get_instance )->run();
+( \WebPImageConverter\WebPImageConverter::get_instance )->run();
