34590f3f381d1780453ba89eda62345705efd6a4
Chigozie Orunta <chigozieorunta@yahoo.com>
2023-03-23 22:21:53 +0100

    Tracking new Settings file

---
+++ src/Plugin/Settings.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Plugin Settings.
+ *
+ * @package WebPImageConverter
+ */
+
+namespace WebPImageConverter\Plugin;
+
+/**
+ * Settings Class.
+ */
+class Settings {
+	/**
+	 * Plugin name.
+	 *
+	 * @const string
+	 */
+	public const NAME = 'WebP Image Converter';
+
+	/**
+	 * Plugin slug.
+	 *
+	 * @const string
+	 */
+	public const SLUG = 'webp-image-converter';
+
+	/**
+	 * Plugin role.
+	 *
+	 * @const string
+	 */
+	public const ROLE = 'manage_options';
+
+	/**
+	 * Plugin domain.
+	 *
+	 * @const string
+	 */
+	public const DOMAIN = 'webp-image-converter';
+}
