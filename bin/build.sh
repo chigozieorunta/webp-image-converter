eecfedd718b8fa08fa5e1ac23d92e37b86e9d57e
Chigozie Orunta <chigozieorunta@yahoo.com>
2023-04-03 12:51:22 +0100

    chore: Bash task for generating plugin build

---
+++ bin/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Set the name of your plugin file
+PLUGIN_FILE="webp-image-converter.php"
+
+# Set the name of the build directory
+BUILD_DIR="build"
+
+# Set the source directories you want to copy
+SRC_DIRS="assets languages src vendor"
+
+# Create the build directory if it doesn't exist
+if [ ! -d "$BUILD_DIR" ]; then
+  mkdir "$BUILD_DIR"
+fi
+
+# Copy the source directories and plugin files to the build directory
+for dir in $SRC_DIRS
+do
+  cp -r "$dir" "$BUILD_DIR/$dir"
+done
+cp "$PLUGIN_FILE" "$BUILD_DIR/$PLUGIN_FILE"
+cp "readme.txt" "$BUILD_DIR/readme.txt"
+
+# Confirm that the files have been copied successfully
+echo "Build complete!"
